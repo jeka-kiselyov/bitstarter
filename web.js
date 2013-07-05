39814c9d254fd8ab511129e3f4f37879a4f74194
jeka-kiselyov <jeka911@gmail.com>
2013-07-05 09:39:50 +0000

    Read index.html as template

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var tpl_file = 'index.html';
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+	var tpl = fs.readFileSync(tpl_file).toString();
+	response.send(tpl);
 });
 
 var port = process.env.PORT || 5000;
