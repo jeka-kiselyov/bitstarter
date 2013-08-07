ed448c285547e31fe6125eed7447cce5052ef5f3
jeka.kiselyov <jeka911@gmail.com>
2013-08-07 12:03:53 +0300

    favicon handling

--- web.js
+++ web.js
@@ -5,9 +5,14 @@ var app = express.createServer(express.logger());
 var tpl_file = 'index.html';
 
 app.get('/', function(request, response) {
-  
 	var tpl = fs.readFileSync(tpl_file).toString();
 	response.send(tpl);
+});
+
+var favicon = fs.readFileSync('favicon.ico');
+app.get('/favicon.ico', function(request, response) {
+	response.setHeader("Content-Type", "image/x-icon");
+	response.send(favicon);
 });
 
 var port = process.env.PORT || 8080;
